1268b415ec47338fa8840cd59de5b3b7505f9acd
Vitor <gabrielvitorbv58@gmail.com>
2019-11-04 21:59:19 -0300

    chore: Create model User and connection to Sequelize with database config

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, resp) => {
-  return resp.json({ message: 'hello word' });
+    return resp.json({ message: 'hello word' });
 });
 
 export default routes;
