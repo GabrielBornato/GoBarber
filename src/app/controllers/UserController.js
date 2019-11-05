7deae9415ccc501ce016997e63f4e05fdec1e367
Vitor <gabrielvitorbv58@gmail.com>
2019-11-05 20:46:52 -0300

    chore: User  Validations

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,8 +2,14 @@ import User from '../models/User';
 
 class UserController {
     async store(req, resp) {
-        const user = await User.create(req.body);
-        return resp.json({ user });
+        const userExist = await User.findOne({
+            where: { email: req.body.email },
+        });
+
+        if (userExist) resp.status(400).json({ error: 'User Already exists.' });
+
+        const { email, name, id, provider } = await User.create(req.body);
+        return resp.json({ email, name, id, provider });
     }
 }
 
